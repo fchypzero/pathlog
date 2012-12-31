0d2258802b02a384c37c51b078d95054c4731d49
pagecho <appcho@gmail.com>
2012-12-31 14:13:06 +0800

    修复函数错误

--- page.php
+++ page.php
@@ -1,19 +1,28 @@
 <?php $this->need('header.php'); ?>
-<section id="content">
-	<article class="post">
+<div id="timeline" class="horizontal"></div>
+<section id="content" class="post_content">
+	<div class="post_item">
+	<div class="post">
+		<article>
 		<header>
 			<h1><?php $this->title() ?></h1>
 		</header>
 		<div class="con">
 			<?php $this->content(''); ?>
 		</div>
-	</article>
-	<div class="postunder">
+		</article>
+		<div class="post_comment">
 		<?php $this->need('comments.php'); ?>
+		</div>
+	</div>
+	
+	<div class="post_bottom">
+			<time><?php $this->date('F d, Y'); ?></time>
+			<span class="premalink">
+				<?php $this->permalink() ?>
+			</span>
+	</div>
 	</div>
 </section>
 <div class="clear"></div>
-<aside class="sidebar">
-	<?php $this->need('sidebar.php'); ?>
-</aside>
 <?php $this->need('footer.php'); ?>
